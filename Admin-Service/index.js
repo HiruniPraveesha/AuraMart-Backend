fc0f518f488c107fe3788222ddd903bbe65f98fb
ThiliniWasana <wthilini418@gmail.com>
2024-11-18 17:43:41 +0530

    productService & Admin dashboard

--- Admin-Service/index.js
+++ Admin-Service/index.js
@@ -3,7 +3,7 @@ import mongoose from "mongoose";
 import cors from "cors";
 import dotenv from "dotenv";
 import morgan from "morgan";
-import ProductRoutes from "./routes/ProductRoutes.js";
+import SellerRoutes from "./routes/SellerRoutes.js";
 import cookieParser from "cookie-parser";
 
 dotenv.config();
@@ -13,6 +13,7 @@ const PARAMS = {
     useNewUrlParser: true, 
     useUnifiedTopology: true
 };
+
 const URI = process.env.MONGOOSE_URI;
 const PORT = process.env.PORT || 5000;
 
@@ -21,10 +22,10 @@ app.use(express.json());
 app.use(cors());
 app.use(cookieParser());
 
-app.use("/api/product", ProductRoutes);
+app.use("/api/seller", SellerRoutes);
 
 mongoose.set("strictQuery", false);
 mongoose.connect(URI, PARAMS)
     .then(() => app.listen(PORT, 
-        () => console.info(`Product Service running on PORT ${PORT} 🔥`)))
+        () => console.info(`Seller Service is running on PORT ${PORT} 🔥`)))
     .catch((err) => console.error(err.message));
